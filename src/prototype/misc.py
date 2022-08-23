aeece2edf68ec830e879af9f0f4a4c3107850d04
root <root@989561-co21302.tmweb.ru>
2022-08-23 10:45:46 +0300

    misc

--- src/prototype/misc.py
+++ src/prototype/misc.py
@@ -1,9 +1,11 @@
 import logging
 from os import getenv
+from dotenv import load_dotenv
 
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
+load_dotenv()
 token = getenv('BOT_TOKEN')
 bot = Bot(token=token)
 memory_storage = MemoryStorage()
